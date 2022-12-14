8813d8f3a80727ba38a39c36b195e61517b2844e
Matthew <matt@pandolfo.com>
2022-12-14 13:24:54 -0500

    picker component

--- App.js
+++ App.js
@@ -1,8 +1,37 @@
 import { useState } from "react";
 import { Text, TextInput } from "react-native";
+import AppPicker from "./app/components/AppPicker";
 import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 
+const categories = [
+  {
+    label: "Furniture",
+    value: 1,
+  },
+  {
+    label: "Clothing",
+    value: 2,
+  },
+  {
+    label: "Cameras",
+    value: 3,
+  },
+];
+
 export default function App() {
-  return <Screen></Screen>;
+  const [category, setCategory] = useState(categories[0]);
+
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
+  );
 }
